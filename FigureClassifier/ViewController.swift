122fff95bf0b0fda859961e3aec8fe53973c9108
Park, Sungmin <ts-sungmin.park@rakuten.com>
2020-03-28 14:19:38 +0900

    Initial Commit

---
+++ FigureClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FigureClassifier
+//
+//  Created by Park, Sungmin on 2020/03/28.
+//  Copyright © 2020 NXT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
